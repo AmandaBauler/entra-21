1151105870c3205bc01e117a5ccc4aff88adbb17
williamlt <william.a.antunesleite@gmail.com>
2022-05-13 21:53:06 -0300

    Iniciando For Incluso exercicio 1 e 2 de for, exercicio 2 ainda não finalizado.

---
+++ Entra21.ExerciciosFor/Exercicio01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosFor
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            var preco = 0.0;
+            var nome = "";
+
+            for (var i = 0; i < 13; i = i + 1)
+            {
+                Console.WriteLine("Nome da peça: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Valor da peça: ");
+                preco = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
