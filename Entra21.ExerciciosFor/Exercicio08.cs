abcdd6b70917a686089d3290433aae8759ba44ab
Amanda Caroline Bauler Mota <amanda.bauler@hotmail.com>
2022-05-15 13:45:27 -0300

    Inclusão Incluido exercicio 08

---
+++ Entra21.ExerciciosFor/Exercicio08.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosFor
+{
+    internal class Exercicio08
+    {
+        public void Executar()
+        {
+            
+            var multiplicador2 = 0;
+            var multiplicando = 0;
+
+            Console.WriteLine("Escolha um numero ");
+            var multiplicador = Convert.ToInt32(Console.ReadLine());
+
+            for (var contador = 0;  contador <= 1000; contador++)
+            {
+                multiplicando = multiplicador * multiplicador2;
+
+                Console.WriteLine(multiplicador + " X " + multiplicador2 + " = " + multiplicando);
+
+                multiplicador2++;
+               
+            }
+
+        }
+    }
+}
