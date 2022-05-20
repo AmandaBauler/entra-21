d0de3c3f69167e56322e4fbb453ecf19b39c090b
AmandaBauler <amanda.bauler@hotmail.com>
2022-05-20 19:13:25 -0300

    Iniciando novo assunto Exercicio 01 de vetor.

---
+++ Entra21.ExerciciosVetor/Exercicio01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            //Crie um vetor para guardar 16 números inteiros
+            //preencha todas as posições com números e apresente a soma destes números.
+
+            int[] numeros  = new int[16];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Escreva um numero: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int soma = 0;
+            
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                soma = soma + numeros[i];
+            }
+
+            Console.WriteLine("A soma dos numeros digitados é: " + soma);
+            
+
+        }
+    }
+}
---
+++ Entra21.ExerciciosVetor/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entra21.ExerciciosVetor;
+
+Console.WriteLine(@"------------Menu-----------
+1- Atividade 01
+2- Atividade 02
+3- Atividade 03
+4- Atividade 04
+5- Atividade 05
+6- Atividade 06
+7- Atividade 07");
+
+Console.WriteLine("Digite uma opção: ");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDesejada == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
