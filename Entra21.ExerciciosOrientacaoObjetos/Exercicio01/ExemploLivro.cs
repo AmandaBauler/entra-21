5f609e17ae687927770d9f904c81391f2fe83bfc
AmandaBauler <amanda.bauler@hotmail.com>
2022-05-26 22:11:14 -0300

    Exercicio 01 Criado exercicio de biblioteca de apenas 1 Livro. *Não finalizado*

---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/ExemploLivro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class ExemploLivro
+    {
+        public void Executar()
+        {
+            var livro = new Livro();
+            livro.Titulo = "As Vantagens de ser Invisivel";
+            livro.Autor = "Stephen Chbosky";
+            livro.CodigoIsbn = "978-1-63-245042-5";
+            livro.QuantidadePaginas = 224;
+            livro.QuantidadePaginasLidas = 112;
+
+            livro.ApresentarTituloAutor();
+            livro.ApresentarQuantidadePaginasParaLer();
+            livro.ApresentarQuantidadePaginasLidasNoTotal();
+
+        }
+    }
+}
---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/Livro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CodigoIsbn;
+        public string IdiomaOriginal;
+        public DateTime DataLancamento;
+        public int QuantidadePaginas;
+        public int QuantidadePaginasLidas;
+        public int QuantidadeReleituras;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine($"CÓDIGO {CodigoIsbn} - {Titulo} - {Autor}");
+        }
+
+        public void ApresentarQuantidadePaginasParaLer()
+        {
+            Console.WriteLine($"Você tera que ler no total {QuantidadePaginas} páginas.");
+        }
+
+        public void ApresentarQuantidadePaginasLidasNoTotal()
+        {
+            Console.WriteLine($"Você leu no total {QuantidadePaginasLidas} páginas.");
+        }
+
+    }
+}
