ed4a6daa47d8367c8de49085d03f11f5aca963e1
Amanda.Mota <amanda.bauler@hotmail.com>
2022-06-06 20:22:30 -0300

    Criado novo projeto Aprendendo Listas. Criado projeto Exercicios Listas. Exercicio 01 finalizado.

---
+++ Entra21.ExerciciosListas/Exercicio01.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosListas
+{
+    public class Exercicio01
+    {
+        public void Executar()
+        {
+            List<string> melhoresFilmes = new List<string>();
+            //Adicionar filmes:
+            melhoresFilmes.Add("Dragon Ball Evollution");
+            melhoresFilmes.Add("Titanic");
+            melhoresFilmes.Add("Lanterna verde");
+            //Apresentar os filmes listados
+            Console.WriteLine("1º filme: " + melhoresFilmes[0] + "\n2º filme: " + melhoresFilmes[1] + "\n3º filme: " + melhoresFilmes[2]);
+            //Adicionar filme
+            melhoresFilmes.Add("");
+            //Alterar filme
+            melhoresFilmes[0] = "Dragon Ball Super: Broly";
+            //Remover filme
+            melhoresFilmes.Remove("Lanterna verde");
+            //Verificar se filme esta na lista
+            bool contemFilme = melhoresFilmes.Contains("Lanterna verde");
+            if (contemFilme == true)
+            {
+                Console.WriteLine("O filme Lanterna Verde esta na lista!");
+            }
+            else
+            {
+                Console.WriteLine("O filme Lanterna verde não esta na lista!");
+            }
+            //Adicionar filme
+            melhoresFilmes.Add("Quarteto fantástico");
+            //Apresentar
+            Console.WriteLine("1º filme: " + melhoresFilmes[0] + "\n2º filme: " + melhoresFilmes[1] + "\n3º filme: " + melhoresFilmes[2] + "\n4º filme: " + melhoresFilmes[3]);
+            //Alterar filme
+            melhoresFilmes[1] = "Titanic 2";
+            //Apresentar
+            Console.WriteLine("1º filme: " + melhoresFilmes[0] + "\n2º filme: " + melhoresFilmes[1] + "\n3º filme: " + melhoresFilmes[2] + "\n4º filme: " + melhoresFilmes[3]);
+            //Verificar se filme esta na lista
+            bool contemFilme1 = melhoresFilmes.Contains("A Lagoa Azul");
+            if (contemFilme1 == true)
+            {
+                Console.WriteLine("O filme lagoa azul esta na lista!");
+            }
+            else
+            {
+                Console.WriteLine("O filme lagoa azul não esta na lista!");
+            }
+
+        }
+
+    }
+}
---
+++ Entra21.ExerciciosListas/Program.cs
@@ -0,0 +1,15 @@
+﻿using Entra21.ExerciciosListas;
+
+Console.WriteLine(@"----------------Menu--------------
+1- Opção 01
+2- Opção 02
+3- Opção 03");
+
+Console.WriteLine("Escolha uma das opções: ");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoDesejada == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
