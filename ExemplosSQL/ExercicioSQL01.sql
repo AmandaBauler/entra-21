2322d46e43c38d05c8b0ea374fedc8e6aca00d2f
amanda.mota <amanda.bauler@hotmail.com>
2022-07-07 19:58:38 -0300

    Finalizado exercicio 01 SQL Incluso exercicio 02 SQL Iniciando exercico 03 SQL

--- ExemplosSQL/ExercicioSQL01.sql
+++ ExemplosSQL/ExercicioSQL01.sql
@@ -1,23 +1,61 @@
-﻿CREATE TABLE pessoas(nome VARCHAR(100), 
+﻿CREATE TABLE pessoas(
+	id INTEGER PRIMARY KEY,
+	nome VARCHAR(100), 
 	cpf VARCHAR(14), 
 	rg VARCHAR(12), 
 	data_nascimento DATETIME2, 
 	idade INTEGER
 );
 
-INSERT INTO pessoas(nome, cpf, rg, data_nascimento, idade)
-	VALUES('Erick Bryan Enrico Vieira', '699.734.958-70', 
-	'90.745.276-0', '1989-05-28', 29);
-INSERT INTO pessoas(nome, cpf, rg, data_nascimento, idade)
-	VALUES('Samuel Ruan Dias', '802.790.194-40', 
-	'55.318.691-7', '08/02/1988 1988-02-08', 30);
---MODIFICAR PARA OS NOMES CORRETOS
-INSERT INTO pessoas(nome, cpf, rg, data_nascimento, idade)
-	VALUES('Erick Bryan Enrico Vieira', '699.734.958-70', 
-	'90.745.276-0', '1989-05-28', 29);
-INSERT INTO pessoas(nome, cpf, rg, data_nascimento, idade)
-	VALUES('Erick Bryan Enrico Vieira', '699.734.958-70', 
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(1, 'Erick Bryan Enrico Vieira', '699.734.958-70', 
 	'90.745.276-0', '1989-05-28', 29);
 
-SELECT nome, cpf, rg, data_nascimento, idade
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(2, 'Samuel Ruan Dias', '802.790.194-40', 
+	'55.318.691-7', '1988-02-08', 30);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(3, 'Fábio Benjamin Souza', '522.445.349-60', 
+	'54.715.232-2', '1970-03-22', 48);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(4, 'Elias Miguel Aparicio', '293.928.821-65', 
+	'12.680.444-8', '1986-06-07', 32);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(5, 'Alexandre Gustavo Cardoso', '617.408.878-24', '8.888.574-4', '2003-04-15', 15);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(6, 'Kauê Lucca Duarte', '943.163.990-47', '46.977.509-9', '1965-06-15', 53);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(7, 'Marcos Bryan Guilherme Mendes', '955.129.235-95', '84.050.077-4', '2015-02-06', 3);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(8, 'Benedito Pedro Carlos da Mota', '510.505.472-50', '43.588.124-3', '1981-04-19', 37);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(9, 'Luís Igor Barbosa', '370.570.311-06', '40.651.407-0', '2013-05-06', 5);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(10, 'José Heitor Ramos', '675.624.925-81', '83.962.258-2', '1965-04-11', 53);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(11, 'Lucca Tiago Galvão', '788.124.508-57', '3.293.335-6', '1977-06-17', 41);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(12, 'Tiago Edson Oliveira', '389.969.352-39', '8.198.446-9', '1977-06-06', 41);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(13, 'Enrico Julio Fábio Martins', '529.078.166-83', '99.886.242-3', '2017-03-11', 1);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(14, 'Oliver Victor Benjamin da Mota', '798.556.805-02', '5.103.578-9', '1985-03-27', 33);
+
+INSERT INTO pessoas(id, nome, cpf, rg, data_nascimento, idade)
+	VALUES(15, 'Bryan Caio Lopes', '213.217.201-30', '12.949.454-9', '1970-06-18', 48);
+
+SELECT id, nome, cpf, rg, data_nascimento, idade
 	FROM pessoas;
---
+++ ExemplosSQL/ExercicioSQL02.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE enderecos (
+	id INTEGER PRIMARY KEY,
+	estado VARCHAR(2),
+	cidade VARCHAR(140),
+	bairro VARCHAR(120),
+	cep VARCHAR(10),
+	logradouro VARCHAR(250),
+	numero INTEGER,
+	complemento TEXT
+);
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(1, 'AC', 'Rio Branco', 'Ayrton Senna', '69.911-866', 
+	'Estrada Deputado José Rui da Silveira Lino', 282, 'Casa');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(2, 'SC', 'Biguaçu', 'Fundos', '88.161-400', 
+	' ', 995, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(3, 'MG', 'Santa Luiza', 'Padre Miguel', '33.082-050', 
+	'Rua Buenos Aires', 371, 'Apartamento');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(4, 'BA', ' ', 'São Tomé de Paripe', '40.800-361', 
+	'Travessa Luiz Hage', 285, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(5, 'MG', 'Ipatinga', 'Vila Celeste', ' ', 
+	'Rua Antonio Boaventura Batista', 645, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(6, 'RS', 'Passo Fundo', 'Nenê Graeff', '99.030-250', 
+	' ', 154, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(7, 'AM', 'Manaus', 'Petrópolis', '69.079-300', 
+	'Rua Coronel Ferreira Sobrinho', 264, 'Fundos');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(8, 'TO', 'Gurupi', 'Muniz Santana', '77.402-130', 
+	'Rua Adelmo Aires Negri', 794, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(9, 'AC', ' ', 'Preventório', ' ', 
+	'Beco da Ligação II', 212, 'Bloco B');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(10, 'AP', 'Santana', 'Comercial', '68.925-073', 
+	'Rua Caiçoene', 648, ' ');
+
+INSERT INTO enderecos(id, estado, cidade, bairro, cep, logradouro, numero, complemento)
+	VALUES(11, 'PB', 'Cabedelo', 'Camalaú', '58.103-052', 
+	'Rua Siqueira Campos', 249, ' ');
+
+SELECT id, estado, cidade, bairro, cep, logradouro, numero, complemento
+	FROM enderecos;
+
